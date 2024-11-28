73a71ac503e41ee2349d905b9fe1430e9f0db3e9
Pirvan Evgheni <eparvan@gmail.com>
2024-11-28 10:16:06 +0200

    A fost adaugat dosarul Models cu entitatile: Event si Particiapnt

---
+++ AppEvenimente/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEvenimente.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string? NumeEveniment { get; set; }
+        public DateTime DataEveniment { get; set; }
+        public string? Locatie { get; set; }
+    }
+}
---
+++ AppEvenimente/Models/Participant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEvenimente.Models
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public string? NumeParticipant { get; set; }
+        public string? Email { get; set; }
+        public int EvenimentId { get; set; }
+    }
+}
